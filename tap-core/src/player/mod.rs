bb79726654c5ad4f82080c04396238244ef6fdcf
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-23 19:15:51 +0200

    feat(player): exported the API and put some const
    The player function are now exposed to the lib, I've also put some basic
    const to get rid of magic number.

---
+++ tap-core/src/player/mod.rs
@@ -0,0 +1,9 @@
+mod class;
+mod progression;
+mod skill;
+
+pub use class::{Player, PlayerClass, Stats};
+
+pub const MAX_LEVEL: u32 = 100;
+pub const BASE_EXP_REQUIRED: u32 = 100;
+pub const HEALTH_MULTIPLIER: u32 = 10;
