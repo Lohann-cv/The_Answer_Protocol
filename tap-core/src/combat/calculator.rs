9b6bcbaf8a77e5401314e57528507aca83ee894b
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-23 18:46:04 +0200

    feat(tap-core: player, combat): created some basic combat logic
    The player is able to make basic or special attack that scale on their
    stats base on their class.
    For now this is pretty simple, in coming update we could create special
    attack with funny mecanics.

---
+++ tap-core/src/combat/calculator.rs
@@ -0,0 +1,18 @@
+use crate::class::Player;
+
+impl Player {
+    pub fn take_damage(&mut self, amount: u32) {
+        if amount >= self.health {
+            self.health = 0;
+        } else {
+            self.health -= amount;
+        }
+    }
+
+    pub fn heal(&mut self, amount: u32) {
+        self.health += amount;
+        if self.health > self.max_health {
+            self.health = self.max_health;
+        }
+    }
+}
---
+++ tap-core/src/player/skill.rs
@@ -0,0 +1,71 @@
+use crate::class::Player;
+use crate::class::PlayerClass;
+use crate::combat::calculator::{heal, take_damage};
+
+impl Player {
+    pub fn attack(&self, target: &mut Player) {
+        match self.class {
+            PlayerClass::Warrior => {
+                let damage = self.stats.strength * 2;
+                target.take_damage(damage);
+            }
+            PlayerClass::Mage => {
+                let damage = self.stats.intelligence * 3;
+                target.take_damage(damage);
+            }
+            PlayerClass::Archer => {
+                let damage = self.stats.agility * 2;
+                target.take_damage(damage);
+            }
+            PlayerClass::Summoner => {
+                let damage = self.stats.intelligence * 2;
+                target.take_damage(damage);
+            }
+            PlayerClass::Healer => {
+                let healing = self.stats.intelligence * 2;
+                self.heal(healing);
+            }
+            PlayerClass::Tank => {
+                let damage = self.stats.stamina * 2;
+                target.take_damage(damage);
+            }
+            PlayerClass::Thief => {
+                let damage = self.stats.agility * 3;
+                target.take_damage(damage);
+            }
+        }
+    }
+
+    pub fn special_ability(&self, target: &mut Player) {
+        match self.class {
+            PlayerClass::Warrior => {
+                let damage = self.stats.strength * 5;
+                target.take_damage(damage);
+            }
+            PlayerClass::Mage => {
+                let damage = self.stats.intelligence * 6;
+                target.take_damage(damage);
+            }
+            PlayerClass::Archer => {
+                let damage = self.stats.agility * 4;
+                target.take_damage(damage);
+            }
+            PlayerClass::Summoner => {
+                let damage = self.stats.intelligence * 5;
+                target.take_damage(damage);
+            }
+            PlayerClass::Healer => {
+                let healing = self.stats.intelligence * 5;
+                self.heal(healing);
+            }
+            PlayerClass::Tank => {
+                let damage = self.stats.stamina * 5;
+                target.take_damage(damage);
+            }
+            PlayerClass::Thief => {
+                let damage = self.stats.agility * 6;
+                target.take_damage(damage);
+            }
+        }
+    }
+}
