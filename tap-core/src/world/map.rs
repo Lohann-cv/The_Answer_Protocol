81e29d8f4098111c0fb3612e774025e8468702db
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-25 08:42:40 +0200

    feat(tap-core: world): add basic world structure
    - Add a 'WorldMap' struct that will act as the blueprint of the world
    - The world is made of 'Room' that hold items, NPCs and quests

--- tap-core/src/world/map.rs
+++ tap-core/src/world/map.rs
@@ -1,4 +1,37 @@
 use serde::{Deserialize, Serialize};
+use crate::items::ItemId;
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct WorldId(pub u32);
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct RoomId(pub u32);
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct WorldMap {
+    id: WorldId,
+    name: str,
+    description: str,
+    rooms: Vector<RoomId>,
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Room {
+    id: RoomId,
+    name: str,
+    description: str,
+    items: Vector<ItemId>,
+    npcs: Vector<NPC>,
+    quests: Vector<Quest>,
+    exits: RoomExit,
+    is_safe: bool,
+    world_exit: bool,
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct RoomExit {
+    pub south: bool,
+    pub north: bool,
+    pub west: bool,
+    pub east: bool,
+}
