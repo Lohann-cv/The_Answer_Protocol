be07ff5934d21e6df6b64fb7ffe8d10832d2ce26
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-25 05:25:43 +0200

    feat(tap-core): Exposed the API in lib.rs

--- tap-core/src/lib.rs
+++ tap-core/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod combat;
+mod player;
+mod world;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use player::{BASE_EXP_REQUIRED, Player, PlayerClass, PlayerId, Stats};
