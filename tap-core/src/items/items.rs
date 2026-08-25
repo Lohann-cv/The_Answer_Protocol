f3983fad6102485ed75b5442ee2f724dda87ee34
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-25 10:49:37 +0200

    feat(tap-core: items): add core item foundation
    - The item can be identify by it's name, or id
    - Yet it's still better to identify it by its 'ItemId' to avoid taking
      all the struct information

--- tap-core/src/items/items.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
-pub struct ItemId(pub u32);
-
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
-pub struct Item {
-    id: ItemId,
-    name: str,
-    description: str,
-}
