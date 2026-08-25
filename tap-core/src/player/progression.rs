2d647cd233a46474caf0acdeff533e912e25da11
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-25 03:10:33 +0200

    docs(tap-core: player): add rust documentation for progression.rs

--- tap-core/src/player/progression.rs
+++ tap-core/src/player/progression.rs
@@ -1,7 +1,35 @@
+//! This module holds the EXP progression logic
+//! The level up is exponentially hard to get
+//! For now in this version the player leftover EXP isn't kept when leveling up
+
 use super::{BASE_EXP_REQUIRED, HEALTH_MULTIPLIER, MAX_LEVEL};
 use super::{Player, PlayerClass, Stats};
 
 impl Player {
+    /// Levels up the player.
+    ///
+    /// This function is called by 'gain_experience' when the needed EXP is reached.
+    /// It also increases the player max health.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tap_core::player::class::{Player, PlayerId, PlayerClass};
+    ///
+    /// // We create a player that is level 1
+    /// let mut player = Player::new(PlayerId(1), PlayerClass::Warrior);
+    /// assert_eq!(player.level, 1);
+    ///
+    /// // We simulate damage
+    /// player.health -= 10;
+    ///
+    /// // Then level up
+    /// player.level_up();
+    ///
+    /// // The level increases and health is fully restored
+    /// assert_eq!(player.level, 2);
+    /// assert_eq!(player.health, player.max_health);
+    /// ```
     pub fn level_up(&mut self) {
         self.level += 1;
         self.experience = 0;
@@ -9,6 +37,28 @@ impl Player {
         self.health = self.max_health;
     }
 
+    /// Make the player gain experience.
+    ///
+    /// It will be exponentially harder for the player to gain a level.
+    /// The player cannot exceed the 'MAX_LEVEL' cap.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tap_core::player::class::{Player, PlayerId, PlayerClass};
+    /// # use tap_core::player::BASE_EXP_REQUIRED;
+    ///
+    /// // We create a player that is level 1
+    /// let mut player = Player::new(PlayerId(1), PlayerClass::Warrior);
+    /// assert_eq!(player.level, 1);
+    ///
+    /// // We give the player enough EXP to make it level up
+    /// player.gain_experience(BASE_EXP_REQUIRED);
+    ///
+    /// // The player is indeed level 2
+    /// assert_eq!(player.level, 2);
+    /// assert_eq!(player.experience, 0);
+    /// ```
     pub fn gain_experience(&mut self, amount: u32) {
         self.experience += amount; // We could add different exp gain depending on the class
         if self.experience >= self.level * BASE_EXP_REQUIRED && self.level < MAX_LEVEL {
