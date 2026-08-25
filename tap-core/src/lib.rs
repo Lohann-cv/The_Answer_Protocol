3ff52b8fdef08db967e950186270af3b056f95a7
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-25 11:07:09 +0200

    chore(tap-core): expose the new element to the API users
    - All the element are expose so we must remove the one that aren't
      needed when the version is up

--- tap-core/src/lib.rs
+++ tap-core/src/lib.rs
@@ -1,5 +1,10 @@
 mod combat;
+mod items;
 mod player;
+mod quest;
 mod world;
 
+pub use items::{Item, ItemId};
 pub use player::{BASE_EXP_REQUIRED, Player, PlayerClass, PlayerId, Stats};
+pub use quest::{Quest, QuestId};
+pub use world::{Npc, NpcId, Room, RoomId, WorldId, WorldMap};
