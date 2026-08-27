678371f35cf05df05b228578bf013369668f3640
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-28 01:38:40 +0200

    feat(tap-core: quest): create 'Quest' foundation
    - Add a creation method for the 'Quest'
    - impl the default trait for 'Quest'

--- tap-core/src/quest/quest_data.rs
+++ tap-core/src/quest/quest_data.rs
@@ -1,11 +1,27 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Default)]
 pub struct QuestId(pub u32);
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Quest {
-    id: QuestId,
-    name: String,
-    description: String,
+    pub id: QuestId,
+    pub name: String,
+    pub description: String,
+}
+
+impl Quest {
+    pub fn new(id: QuestId, name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self {
+            id,
+            name: name.into(),
+            description: description.into(),
+        }
+    }
+}
+
+impl Default for Quest {
+    fn default() -> Self {
+        Self::new(QuestId::default(), "Blank Quest", "A blank quest")
+    }
 }
