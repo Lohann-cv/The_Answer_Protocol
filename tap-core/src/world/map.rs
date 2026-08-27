39346cfa421c8ca27a2b132dc935e31149097475
Lohann-cv <lohanncoantverdier@gmail.com>
2026-08-28 01:16:52 +0200

    feat(tap-core: world): add world and room logic as well as a manager
    - add 'WorldMap' that is identify with its 'WorldId' it hold a vec of 'Room'
    - create the 'Room' core foundation that hold 'Item', 'Npc' and
      'RoomExit' holding the cardinal coordinate that lead to another 'Room'
    - implement all the base method for 'WorldMap' and 'Room' as well as
      their default trait
    - create a 'WorldStock' that act as the manager of the world, it hold
      hash map to give reference to 'Room' and 'WorldMap' struct based on
      the Id that is give in parameters

--- tap-core/src/world/map.rs
+++ tap-core/src/world/map.rs
@@ -1,12 +1,12 @@
 use super::NpcId;
 use crate::items::ItemId;
-use crate::quest::QuestId;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq, Default, Hash)]
 pub struct WorldId(pub u32);
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq, Default, Hash)]
 pub struct RoomId(pub u32);
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
@@ -24,16 +24,122 @@ pub struct Room {
     pub description: String,
     pub items: Vec<ItemId>,
     pub npcs: Vec<NpcId>,
-    pub quests: Vec<QuestId>,
     pub exits: RoomExit,
     pub is_safe: bool,
-    pub world_exit: bool,
 }
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct RoomExit {
-    pub south: bool,
-    pub north: bool,
-    pub west: bool,
-    pub east: bool,
+    pub south: Option<RoomId>,
+    pub north: Option<RoomId>,
+    pub west: Option<RoomId>,
+    pub east: Option<RoomId>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Default)]
+pub struct WorldStock {
+    worlds: HashMap<WorldId, WorldMap>,
+    rooms: HashMap<RoomId, Room>,
+}
+
+impl WorldMap {
+    pub fn new(
+        id: WorldId,
+        name: impl Into<String>,
+        description: impl Into<String>,
+        rooms: impl IntoIterator<Item = RoomId>,
+    ) -> Self {
+        Self {
+            id,
+            name: name.into(),
+            description: description.into(),
+            rooms: rooms.into_iter().collect(),
+        }
+    }
+}
+
+impl Room {
+    pub fn new(
+        id: RoomId,
+        name: impl Into<String>,
+        description: impl Into<String>,
+        items: impl IntoIterator<Item = ItemId>,
+        npcs: impl IntoIterator<Item = NpcId>,
+        exits: RoomExit,
+        is_safe: bool,
+    ) -> Self {
+        Self {
+            id,
+            name: name.into(),
+            description: description.into(),
+            items: items.into_iter().collect(),
+            npcs: npcs.into_iter().collect(),
+            exits,
+            is_safe,
+        }
+    }
+}
+
+impl WorldStock {
+    pub fn new(
+        worlds: impl IntoIterator<Item = WorldMap>,
+        rooms: impl IntoIterator<Item = Room>,
+    ) -> Self {
+        Self {
+            worlds: worlds.into_iter().map(|w| (w.id, w)).collect(),
+            rooms: rooms.into_iter().map(|r| (r.id, r)).collect(),
+        }
+    }
+
+    pub fn add_room(&mut self, room: Room) -> Option<Room> {
+        self.rooms.insert(room.id, room)
+    }
+
+    pub fn remove_room(&mut self, id: RoomId) -> Option<Room> {
+        self.rooms.remove(&id)
+    }
+
+    pub fn get_room(&self, id: RoomId) -> Option<&Room> {
+        self.rooms.get(&id)
+    }
+
+    pub fn get_room_mut(&mut self, id: RoomId) -> Option<&mut Room> {
+        self.rooms.get_mut(&id)
+    }
+
+    pub fn add_world(&mut self, world: WorldMap) -> Option<WorldMap> {
+        self.worlds.insert(world.id, world)
+    }
+
+    pub fn remove_world(&mut self, id: WorldId) -> Option<WorldMap> {
+        self.worlds.remove(&id)
+    }
+
+    pub fn get_world(&self, id: WorldId) -> Option<&WorldMap> {
+        self.worlds.get(&id)
+    }
+
+    pub fn empty() -> Self {
+        Self::default()
+    }
+}
+
+impl Default for WorldMap {
+    fn default() -> Self {
+        Self::new(WorldId::default(), "Blank World", "A blank world", [])
+    }
+}
+
+impl Default for Room {
+    fn default() -> Self {
+        Self::new(
+            RoomId::default(),
+            "Blank Room",
+            "A blank room",
+            [],
+            [],
+            RoomExit::default(),
+            false,
+        )
+    }
 }
